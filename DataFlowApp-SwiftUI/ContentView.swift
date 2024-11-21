8bc607019a6b8c2b21459cead3c37df26e796739
ahmetbostanciklioglu <ahmetbostancikli@gmail.com>
2024-11-22 01:09:17 +0300

    initial Commit

--- DataFlowApp-SwiftUI/ContentView.swift
+++ DataFlowApp-SwiftUI/ContentView.swift
@@ -1,19 +1,48 @@
-//
-//  ContentView.swift
-//  DataFlowApp-SwiftUI
-//
-//  Created by Ahmet Bostancıklıoğlu on 22.11.2024.
-//
-
 import SwiftUI
+import Observation
+
+@Observable class User {
+    var name: String = ""
+    var jobTitle: String = ""
+    var followerCount: Int = 0
+    var bio: String = ""
+    
+    init(name: String, jobTitle: String, followerCount: Int, bio: String) {
+        self.name = name
+        self.jobTitle = jobTitle
+        self.followerCount = followerCount
+        self.bio = bio
+    }
+}
+
 
 struct ContentView: View {
+    
+     var user = User(name: "Ahmet",
+                                 jobTitle: "Android Dev",
+                                 followerCount: 3,
+                                 bio: "My bio goes here")
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack(alignment: .leading) {
+            Text(user.name)
+                .font(.title.bold())
+            
+            Text(user.jobTitle)
+                .foregroundStyle(.secondary)
+            
+            Text("\(user.followerCount) Followers")
+                .foregroundStyle(.secondary)
+            
+            Button {
+                user.followerCount += 1
+            } label: {
+                Text("Add Follower")
+            }
+            .buttonStyle(.borderedProminent)
+            .padding(.top, 30)
+            
+            
         }
         .padding()
     }
