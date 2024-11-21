381c14711b2e3247dcef8bea7358884ee363e648
ahmetbostanciklioglu <ahmetbostancikli@gmail.com>
2024-11-22 00:53:04 +0300

    Initial Commit

---
+++ DataFlowApp-SwiftUI/DataFlowApp_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  DataFlowApp_SwiftUIApp.swift
+//  DataFlowApp-SwiftUI
+//
+//  Created by Ahmet Bostancıklıoğlu on 22.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct DataFlowApp_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
